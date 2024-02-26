2e62018c3bd47f3de6c010ae0aca594582a4de9f
JuntaLaw <junta_law@me.com>
2024-02-26 13:04:52 +0900

    add shadcn ui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import { GeistSans } from "geist/font/sans";
 import { Noto_Sans_JP } from "next/font/google";
+import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
+
+import { cn } from "../lib/utils";
+
+export const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -19,7 +27,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ja" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      // className="bg-background text-foreground"
+      >
         <main className="min-h-screen flex flex-col items-center">
           {children}
         </main>
--- app/toppage/page.tsx
+++ app/toppage/page.tsx
@@ -4,6 +4,7 @@ import { createClient } from "@/utils/supabase/server";
 import FetchDataSteps from "@/components/tutorial/FetchDataSteps";
 import Header from "@/components/lp/LpTop";
 import { redirect } from "next/navigation";
+import Image from "next/image";
 
 export default async function ProtectedPage() {
     const supabase = createClient();
@@ -17,25 +18,24 @@ export default async function ProtectedPage() {
     }
 
     return (
-        <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        <div className="flex-1 w-full flex flex-col gap-10 items-center">
             <div className="w-full">
-                <div className="py-6 font-bold bg-purple-950 text-white text-center">
-                    This is a protected page that you can only see as an authenticated
-                    user
-                </div>
+
                 <nav className="w-full flex justify-between border-b border-b-foreground/10 h-16">
-                    <p>logo area</p>
-                    <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
 
-                        <AuthButton />
-
+                    <div className="w-full max-w-4xl flex justify-start items-center p-3 text-sm">
+                        test
                     </div>
+                    <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
+                        <AuthButton />
+                    </div>
+
                 </nav>
             </div>
 
             <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
 
-                <main className="flex-1 flex flex-col gap-6">
+                <main className="flex-1 flex flex-col gap-2">
                     <h2 className="font-bold text-4xl mb-4">Next steps</h2>
                     <p>main area</p>
                 </main>
