4a3ecb487f6ad87ded17f40bc21982c3e259b4c3
JuntaLaw <junta_law@me.com>
2024-02-26 13:53:47 +0900

    add daisy ui

--- app/page.tsx
+++ app/page.tsx
@@ -24,6 +24,7 @@ export default async function Index() {
       <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
         <div className="w-full max-w-4xl flex justify-end items-center p-3 text-sm">
           {isSupabaseConnected && <AuthButton />}
+          <button className="btn">Button</button>
         </div>
       </nav>
 
