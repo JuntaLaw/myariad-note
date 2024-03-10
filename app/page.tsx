6a4474867b4697c45e84a10703137ab92f01701f
JuntaLaw <junta_law@me.com>
2024-03-10 17:36:38 +0900

    不要なページの削除

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,5 @@
-import DeployButton from "../components/DeployButton";
 import AuthButton from "../components/AuthButton";
 import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/tutorial/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/tutorial/SignUpUserSteps";
 import LpTop from "@/components/lp/LpTop";
 
 export default async function Index() {
