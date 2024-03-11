7ec9f3863464c4b9966fa0c0cc08828186c228cf
JuntaLaw <junta_law@me.com>
2024-03-11 19:11:33 +0900

    notepage update

--- app/notepage/page.tsx
+++ app/notepage/page.tsx
@@ -2,32 +2,41 @@
 import Sidebar from "@/components/navi/Sidebar";
 import Notebook from "@/components/ui/Card/Notebook";
 import CardNote from "@/components/ui/Card/CardNote";
-// import { createClient } from "@/utils/supabase/server";
-// import { redirect } from "next/navigation";
-import { useEffect, useState } from 'react';
-
-
+import DraftArea from "@/components/ui/Notepage/DraftArea";
+import CardEditArea from "@/components/ui/Notepage/CardEditArea";
 export default async function () {
-
-
     return (
-        <div className="flex w-full">
-            <div>
-                <nav className="flex justify-start h-screen">
-                    <Sidebar />
-                </nav>
+        <>
+            <style jsx>{`
+                .page-bottom {
+                    background-color: #cbd5e1; /* Slate-400 */
+                    height: 33.33%;
+                    width: 100%;
+                    position: absolute;
+                    bottom: 0;
+                }
+            `}</style>
+            <div className="page-bottom"></div>
+            <div className="flex w-full">
+                <div>
+                    <nav className="flex justify-start h-screen z-10">
+                        <Sidebar />
+                    </nav>
+                </div>
+                <div>
+                    <main>
+                        <div>
+                            <CardEditArea />
+                        </div>
+                        <div className="ml-10 mt-10 flex gap-10">
+                            <CardNote />
+                            <CardNote />
+                            <CardNote />
+                            <CardNote />
+                        </div>
+                    </main>
+                </div>
             </div>
-
-            <div>
-
-                <main className="ml-10 mt-10 flex gap-10">
-                    <CardNote />
-                    <CardNote />
-                    <CardNote />
-                </main>
-            </div>
-
-
-        </div>
+        </>
     );
 }
---
+++ components/ui/Notepage/CardEditArea.tsx
@@ -0,0 +1,14 @@
+import CardNote from "@/components/ui/Card/CardNote";
+
+const CardEditArea = () => {
+    return (
+        <div className="w-full h-full">
+            <div className="ml-10 mt-10 flex gap-10">
+                <CardNote />
+                <CardNote />
+                <CardNote />
+            </div>
+        </div>
+    );
+};
+export default CardEditArea;
